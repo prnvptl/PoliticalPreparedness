6da0a72725c2d64974532b2042a1640821a88603
Pranav-794 <pranavvpatell@gmail.com>
2022-04-07 17:08:10 -0400

    Handling elections api response edge case

--- app/src/main/java/com/example/android/politicalpreparedness/data/network/jsonadapter/ElectionAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/data/network/jsonadapter/ElectionAdapter.kt
@@ -6,18 +6,23 @@ import com.squareup.moshi.ToJson
 
 class ElectionAdapter {
     @FromJson
-    fun divisionFromJson (ocdDivisionId: String): Division {
+    fun divisionFromJson(ocdDivisionId: String): Division {
         val countryDelimiter = "country:"
         val stateDelimiter = "state:"
-        val country = ocdDivisionId.substringAfter(countryDelimiter,"")
+        val country = ocdDivisionId.substringAfter(countryDelimiter, "")
+            .substringBefore("/")
+        var state = ocdDivisionId.substringAfter(stateDelimiter, "")
+            .substringBefore("/")
+        if (state.isBlank()) {
+            val disctrictDelimiter = "district:"
+            state = ocdDivisionId.substringAfter(disctrictDelimiter, "")
                 .substringBefore("/")
-        val state = ocdDivisionId.substringAfter(stateDelimiter,"")
-                .substringBefore("/")
+        }
         return Division(ocdDivisionId, country, state)
     }
 
     @ToJson
-    fun divisionToJson (division: Division): String {
+    fun divisionToJson(division: Division): String {
         return division.id
     }
 }
